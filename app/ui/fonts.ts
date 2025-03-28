fd92cf4b049ea358c83a1d36b502c166c054eb99
Vince <me@vincerussell.com>
2025-03-28 15:51:45 -0400

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter } from 'next/font/google';
+import { Michroma } from 'next/font/google';
+export const inter = Inter({ subsets: ['latin'] });
+export const michromanter = Michroma({ weight: ['400'], subsets: ['latin'] });
