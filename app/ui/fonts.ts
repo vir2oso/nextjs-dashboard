b92718f70fcc3d736ce4a6a12ca27947b751f4a2
Vince <me@vincerussell.com>
2025-03-28 16:00:23 -0400

    add subset

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -4,6 +4,4 @@ import { Michroma } from 'next/font/google';
 import { Lusitana } from 'next/font/google';
 export const inter = Inter({ subsets: ['latin'] });
 export const michromanter = Michroma({ weight: ['400'], subsets: ['latin'] });
-export const lusitana = Lusitana({
-  weight: ['400', '700'], // Specify the weights you want to use
-});
+export const lusitana = Lusitana({ weight: ['400'], subsets: ['latin'] });
